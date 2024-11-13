c0fef503bd3d5273adabd73cac22582290248f51
JFronny <git@akbvopenfl1.hopto.org>
2024-11-13 18:06:26 +0100

    fix: prevent ClassCastException in dev

--- src/main/java/edu/kit/hci/soli/JteConfiguration.java
+++ src/main/java/edu/kit/hci/soli/JteConfiguration.java
@@ -2,19 +2,21 @@ package edu.kit.hci.soli;
 
 import gg.jte.*;
 import gg.jte.resolve.DirectoryCodeResolver;
+import gg.jte.springframework.boot.autoconfigure.JteProperties;
 import gg.jte.springframework.boot.autoconfigure.JteViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class JteConfiguration {
 
     @Bean
-    public ViewResolver jteViewResolve(TemplateEngine templateEngine) {
-        return new JteViewResolver(templateEngine, ".jte");
+    public ViewResolver jteViewResolve(TemplateEngine templateEngine, JteProperties properties) {
+        return new JteViewResolver(templateEngine, properties);
     }
 
     @Bean
@@ -24,7 +26,12 @@ public class JteConfiguration {
             return TemplateEngine.createPrecompiled(ContentType.Html);
         } else {
             CodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src", "main", "jte"));
-            TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+            TemplateEngine templateEngine = TemplateEngine.create(
+                    codeResolver,
+                    Paths.get("jte-classes"),
+                    ContentType.Html,
+                    SoliApplication.class.getClassLoader()
+            );
             templateEngine.setBinaryStaticContent(true);
             return templateEngine;
         }
