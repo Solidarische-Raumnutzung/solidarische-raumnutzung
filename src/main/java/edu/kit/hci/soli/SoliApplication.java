e5f9ae637c845f944174faa9ffad0f6b0123db0b
JFronny <git@akbvopenfl1.hopto.org>
2024-11-15 19:08:22 +0100

    fix: ensure consistent locale

--- src/main/java/edu/kit/hci/soli/SoliApplication.java
+++ src/main/java/edu/kit/hci/soli/SoliApplication.java
@@ -3,10 +3,13 @@ package edu.kit.hci.soli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class SoliApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(SoliApplication.class, args);
     }
 }
