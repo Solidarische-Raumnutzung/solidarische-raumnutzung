1068199a4d6377e38470d0a39a2abbed7c41c418
JFronny <git@akbvopenfl1.hopto.org>
2024-11-25 14:27:13 +0100

    chore: bump deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
-    implementation("org.flywaydb:flyway-core:10.21.0") // TODO: remove explicit version when Spring Boot 3.4 is released
-    implementation("org.flywaydb:flyway-database-postgresql:10.21.0")
+    implementation("org.flywaydb:flyway-core:11.0.0") // TODO: remove explicit version when the Spring Boot version is updated
+    implementation("org.flywaydb:flyway-database-postgresql:11.0.0")
 
     implementation("org.springframework.boot:spring-boot-starter-mail")
 
