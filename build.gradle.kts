8cf1660ab8843fa5601d6a2a30c9a3aa6aa2a4f6
Jan8192 <uouyo@student.kit.edu>
2024-11-25 15:08:07 +0100

    feat(application): basic setup of oauth2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
     id("gg.jte.gradle") version "3.1.15"
+    id("jacoco")
+
 }
 
 group = "edu.kit.hci"
@@ -11,6 +13,12 @@ version = "0.0.1-SNAPSHOT"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
     }
 }
 
