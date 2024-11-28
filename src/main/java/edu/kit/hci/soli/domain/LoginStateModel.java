7174efd95d78565053ee3d3495cd731e21462cc8
Alexander Klee <alexander.m.klee@gmail.com>
2024-11-28 17:52:42 +0100

    feat: save usertype and save access to CSRF token

--- src/main/java/edu/kit/hci/soli/domain/LoginStateModel.java
+++ src/main/java/edu/kit/hci/soli/domain/LoginStateModel.java
@@ -1,11 +1,25 @@
 package edu.kit.hci.soli.domain;
 
+import org.springframework.security.web.csrf.CsrfToken;
+
 public class LoginStateModel {
     public String name;
     public long visits;
+    public Kind kind;
+    public CsrfToken csrfToken;
 
-    public LoginStateModel(String name, long visits) {
+
+    public LoginStateModel(String name, long visits, Kind kind, CsrfToken csrfToken) {
         this.name = name;
         this.visits = visits;
+        this.kind = kind;
+        this.csrfToken = csrfToken;
+    }
+
+    public enum Kind {
+        VISITOR,
+        OAUTH,
+//        GUEST,
+        ADMIN
     }
 }
