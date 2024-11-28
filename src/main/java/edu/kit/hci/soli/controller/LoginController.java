72f919ad3918f25015782cc881a4c7eafbb2cd94
Alexander Klee <alexander.m.klee@gmail.com>
2024-11-28 17:50:41 +0100

    feat: hide admin username input

--- src/main/java/edu/kit/hci/soli/controller/LoginController.java
+++ src/main/java/edu/kit/hci/soli/controller/LoginController.java
@@ -1,6 +1,7 @@
 package edu.kit.hci.soli.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    @Value("${soli.administrator.name}")
+    private String adminName;
+
     @GetMapping("/login")
     public String login(HttpServletRequest request, Model model) {
         String error = request.getParameter("error");
         String logout = request.getParameter("logout");
+
+        model.addAttribute("adminName", adminName);
 
         if (error != null) {
             model.addAttribute("error", "Invalid username or password");
