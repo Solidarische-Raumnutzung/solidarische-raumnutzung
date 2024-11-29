371e38cdfcfe50955436ff0aa6f297792b0db13f
J. Fronny <github@akbvopenfl1.hopto.org>
2024-11-29 15:18:23 +0100

    Discard changes to src/main/java/edu/kit/hci/soli/controller/BookingsController.java

--- src/main/java/edu/kit/hci/soli/controller/BookingsController.java
+++ src/main/java/edu/kit/hci/soli/controller/BookingsController.java
@@ -1,7 +1,7 @@
 package edu.kit.hci.soli.controller;
 
 
-import edu.kit.hci.soli.dto.LoginStateModel;
+import edu.kit.hci.soli.domain.LoginStateModel;
 import edu.kit.hci.soli.repository.VisitsRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +19,7 @@ public class BookingsController {
     private VisitsRepository visitsRepository;
 
     @GetMapping("/bookings")
-    public String bookings(Model model, HttpServletResponse response, Principal principal, @AuthenticationPrincipal OidcUser oidcUser) {
-
-
-
+    public String bookings(Model model, HttpServletResponse response, Principal principal) {
         return "bookings";
     }
 }
