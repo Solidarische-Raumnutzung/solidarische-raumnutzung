29bdc6592549be20312e91103d75595ebb217a0d
Jan8192 <uouyo@student.kit.edu>
2024-12-01 15:31:53 +0100

    feat: added delete booking feature

--- src/main/java/edu/kit/hci/soli/dto/KnownError.java
+++ src/main/java/edu/kit/hci/soli/dto/KnownError.java
@@ -6,7 +6,8 @@ public enum KnownError {
     NOT_FOUND("error.404.title", "error.404.message"),
     NO_USER("error.bad_login_method.title", "error.bad_login_method.message"),
     INVALID_TIME("error.invalid_time.title", "error.invalid_time.message"),
-    MISSING_PARAMETER("error.missing_parameter.title", "error.missing_parameter.message"),;
+    MISSING_PARAMETER("error.missing_parameter.title", "error.missing_parameter.message"),
+    UNABLE_TO_DELETE_OTHERS_USER_BOOKING("error.unable_to_delete_others_user_booking.title", "error.unable_to_delete_others_user_booking.message"),;
 
     public final @PropertyKey(resourceBundle = "messages") String title;
     public final @PropertyKey(resourceBundle = "messages") String message;
