3a6268b2f3f6efd165e7a5cbfae22d55a02d6b1d
JFronny <git@akbvopenfl1.hopto.org>
2024-12-04 14:50:59 +0100

    style: clean up BookingAttemptResult

--- src/main/java/edu/kit/hci/soli/dto/BookingAttemptResult.java
+++ src/main/java/edu/kit/hci/soli/dto/BookingAttemptResult.java
@@ -6,20 +6,15 @@ import edu.kit.hci.soli.service.BookingsService;
 import java.util.List;
 
 public sealed interface BookingAttemptResult {
-    record Success(Booking booking) implements BookingAttemptResult {
-    }
+    record Success(Booking booking) implements BookingAttemptResult { }
 
-    record Staged(Booking booking) implements BookingAttemptResult {
-    }
+    record Staged(Booking booking) implements BookingAttemptResult { }
 
-    record Failure(List<Booking> conflict) implements BookingAttemptResult {
-    }
+    record Failure(List<Booking> conflict) implements BookingAttemptResult { }
 
     sealed interface PossibleCooperation extends BookingAttemptResult {
         List<Booking> override();
-
         List<Booking> cooperate();
-
         List<Booking> contact();
 
         record Immediate(List<Booking> override, List<Booking> cooperate) implements PossibleCooperation {
@@ -29,8 +24,6 @@ public sealed interface BookingAttemptResult {
             }
         }
 
-        record Deferred(List<Booking> override, List<Booking> contact,
-                        List<Booking> cooperate) implements PossibleCooperation {
-        }
+        record Deferred(List<Booking> override, List<Booking> contact, List<Booking> cooperate) implements PossibleCooperation { }
     }
 }
