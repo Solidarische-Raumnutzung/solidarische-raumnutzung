fc745760fec5dcaf3ecf4c8fa8d8c1fc5d16c6c7
Jan8192 <uouyo@student.kit.edu>
2024-12-11 14:05:15 +0100

    fix: added to equals and hashcode

--- src/main/java/edu/kit/hci/soli/domain/Room.java
+++ src/main/java/edu/kit/hci/soli/domain/Room.java
@@ -34,26 +34,11 @@ public class Room {
     }
 
     public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Room)) return false;
-        final Room other = (Room) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Room;
+        return o instanceof Room r && getId().equals(r.getId());
     }
 
     public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        return result;
+       return getId().hashCode();
     }
 
     public String toString() {
