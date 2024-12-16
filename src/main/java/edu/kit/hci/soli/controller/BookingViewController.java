e93881f066c54398465a34e6b58f34b5624055f5
JFronny <git@akbvopenfl1.hopto.org>
2024-12-16 19:39:23 +0100

    fix: correct imports

--- src/main/java/edu/kit/hci/soli/controller/BookingViewController.java
+++ src/main/java/edu/kit/hci/soli/controller/BookingViewController.java
@@ -13,9 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 import java.util.Optional;
