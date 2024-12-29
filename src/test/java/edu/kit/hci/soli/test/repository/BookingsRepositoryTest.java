2504609bec7434050915bcf295f35331bf3220a8
JFronny <git@akbvopenfl1.hopto.org>
2024-12-29 13:54:19 +0100

    test: get tests running again

--- src/test/java/edu/kit/hci/soli/test/repository/BookingsRepositoryTest.java
+++ src/test/java/edu/kit/hci/soli/test/repository/BookingsRepositoryTest.java
@@ -1,15 +1,20 @@
 package edu.kit.hci.soli.test.repository;
 
 import edu.kit.hci.soli.domain.Booking;
+import edu.kit.hci.soli.domain.ShareRoomType;
+import edu.kit.hci.soli.dto.BookingAttemptResult;
 import edu.kit.hci.soli.repository.BookingsRepository;
+import edu.kit.hci.soli.service.BookingsService;
 import edu.kit.hci.soli.test.TestService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -21,6 +26,8 @@ public class BookingsRepositoryTest {
     private Booking testBooking;
     private Booking testBooking2;
     private Booking testBooking3;
+    @Autowired
+    private BookingsService bookingsService;
 
     @BeforeAll
     public static void clean(@Autowired TestService testService) {
@@ -40,7 +47,12 @@ public class BookingsRepositoryTest {
     }
 
     @Test
+    @Transactional
     public void testDeleteAllBookingsByGuests() {
+        bookingsRepository.save(testBooking);
+        bookingsRepository.save(testBooking2);
+        bookingsRepository.save(testBooking3);
+        assertEquals(3, bookingsRepository.count());
         bookingsRepository.deleteAllBookingsByGuests();
         assertEquals(1, bookingsRepository.count());
     }
