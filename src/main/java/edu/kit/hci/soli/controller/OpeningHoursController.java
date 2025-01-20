b94eef12cf7684fb68d4cea51e01d5b14a6d2b08
ufikl <ufikl@student.kit.edu>
2025-01-20 14:01:23 +0100

    draft: merged master into this && fixed Servlet error in OpeningHoursController.java

--- src/main/java/edu/kit/hci/soli/controller/OpeningHoursController.java
+++ src/main/java/edu/kit/hci/soli/controller/OpeningHoursController.java
@@ -1,18 +1,21 @@
 package edu.kit.hci.soli.controller;
 
+import edu.kit.hci.soli.dto.KnownError;
 import edu.kit.hci.soli.service.RoomService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
 
-@Controller("/admin/opening-hours")
+@Controller
+@RequestMapping("/admin/opening-hours")
 public class OpeningHoursController {
     private final RoomService roomService;
 
@@ -20,17 +23,20 @@ public class OpeningHoursController {
         this.roomService = roomService;
     }
 
-    @GetMapping("/admin/opening-hours/{roomId}/save")
-    public String saveOpeningHours(@PathVariable Long roomId, @RequestParam String startTime, @RequestParam String endTime, @RequestParam DayOfWeek dayOfWeek, Model model, HttpServletResponse response) {
+    @GetMapping("/{roomId}/save")
+    public String saveOpeningHours(@PathVariable Long roomId, @RequestParam String startTime,
+                                   @RequestParam String endTime, @RequestParam DayOfWeek dayOfWeek, Model model,
+                                   HttpServletResponse response) {
+        LocalTime start, end;
         try {
-            LocalTime start = LocalTime.parse(startTime);
-            LocalTime end = LocalTime.parse(endTime);
-            roomService.saveOpeningHours(roomId, start, end, dayOfWeek);
-            return "redirect:/admin/opening-hours/{roomId}";
+            start = LocalTime.parse(startTime);
+            end = LocalTime.parse(endTime);
         } catch (DateTimeParseException e) {
-            model.addAttribute("error", "Invalid time format");
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            model.addAttribute("error", KnownError.INVALID_TIME);
             return "error/known";
         }
+        roomService.saveOpeningHours(roomId, start, end, dayOfWeek);
+        return "redirect:/admin/opening-hours/{roomId}";
     }
 }
