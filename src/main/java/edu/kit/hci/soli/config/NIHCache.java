41c245c26d35f78e54894991678e6e1b8a15158d
ufikl <ufikl@student.kit.edu>
2025-01-29 13:13:15 +0100

    docs: enhance Javadoc comments for NIHCache + formatted

--- src/main/java/edu/kit/hci/soli/config/NIHCache.java
+++ src/main/java/edu/kit/hci/soli/config/NIHCache.java
@@ -2,6 +2,11 @@ package edu.kit.hci.soli.config;
 
 import org.springframework.util.function.ThrowingSupplier;
 
+/**
+ * A cache implementation that updates its value after a specified duration.
+ *
+ * @param <T> the type of the cached value
+ */
 public class NIHCache<T> implements ThrowingSupplier<T> {
     private final long duration;
     private final ThrowingSupplier<T> supplier;
@@ -9,16 +14,22 @@ public class NIHCache<T> implements ThrowingSupplier<T> {
     private long lastWrite = 0;
     private T value;
 
+    /**
+     * Constructs a new NIHCache with the specified duration and supplier.
+     *
+     * @param duration the duration after which the cache should be updated
+     * @param supplier the supplier to fetch the new value
+     */
     public NIHCache(long duration, ThrowingSupplier<T> supplier) {
         this.duration = duration;
         this.supplier = supplier;
     }
 
     /**
-     * Get the element value, may update cache if duration expired.
+     * Gets the cached value, updating the cache if the duration has expired.
      *
-     * @return The value
-     * @throws Exception if cache update failed
+     * @return the cached value
+     * @throws Exception if the cache update fails
      */
     @Override
     public T getWithException() throws Exception {
